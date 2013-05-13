2a7957652816527ca60817bd66134e015bf25ba1
Jifeng <jifeng@kooboo.com>
2013-05-13 17:06:46 +0800

    Fixed filesize validation issue.

--- Kooboo.CMS/Kooboo.CMS.Web/Scripts/Kooboo_CMS.js
+++ Kooboo.CMS/Kooboo.CMS.Web/Scripts/Kooboo_CMS.js
@@ -1024,10 +1024,15 @@ function parse_JsonResultData(response, statusText, xhr, $form) {
     (function jquery_validation_filesize() {
         if ($.validator) {
             $.validator.addMethod('filesize', function (value, element, param) {
-                // param = size (en bytes) 
-                // element = element to validate (<input>)
-                // value = value of the element (file name)
-                return this.optional(element) || (element.files[0].size <= param)
+                if (element.files && element.files.length > 0) {
+                    // param = size (en bytes) 
+                    // element = element to validate (<input>)
+                    // value = value of the element (file name)
+                    return this.optional(element) || (element.files[0].size <= param)
+                }
+                else {
+                    return true;
+                }
             });
             $.validator.unobtrusive.adapters.addSingleVal("filesize", "value");
         }

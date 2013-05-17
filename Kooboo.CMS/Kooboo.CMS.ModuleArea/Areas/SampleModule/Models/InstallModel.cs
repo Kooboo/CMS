500565c3aa600ea0fd81c02373b66d39dbe72e14
Jifeng <jifeng@kooboo.com>
2013-05-17 14:27:38 +0800

    Add the missed files in the comment #4efabe580b22e0c42a5598071deae9ce2d775ed0

---
+++ Kooboo.CMS/Kooboo.CMS.ModuleArea/Areas/SampleModule/Models/InstallModel.cs
@@ -0,0 +1,24 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Kooboo.CMS.ModuleArea.Areas.SampleModule.Models
+{
+    public class InstallModel
+    {
+        public string DatabaseServer { get; set; }
+        public string UserName { get; set; }
+        [UIHint("Password")]
+        public string Password { get; set; }
+    }
+}

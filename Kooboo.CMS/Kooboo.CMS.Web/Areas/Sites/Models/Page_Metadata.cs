ed080e60c704ee7fc8b20c91f4d993ca3389aa7c
Jifeng <jifeng@kooboo.com>
2013-05-29 14:41:39 +0800

    Issue #4, but still need some style changes by jianghao

--- Kooboo.CMS/Kooboo.CMS.Web/Areas/Sites/Models/Page_Metadata.cs
+++ Kooboo.CMS/Kooboo.CMS.Web/Areas/Sites/Models/Page_Metadata.cs
@@ -168,33 +168,7 @@ namespace Kooboo.CMS.Web.Areas.Sites.Models
         [Display(Name = "External URL")]
         public string ExternalUrl { get; set; }
     }
-
-    [MetadataFor(typeof(HtmlMeta))]
-    public class HtmlMeta_Metadata
-    {
-        [Display(Name = "Html title")]
-        [Description("The title tag and value that appear in the page HTML header and also used by browsers as the page title. <br />This value can be accessed via API: @Html.FrontHtml().HtmlTitle()")]
-        public string HtmlTitle { get; set; }
-
-        [Description("Used in SEO, see: http://googlewebmastercentral.blogspot.com/2009/02/specify-your-canonical.html")]
-        public string Canonical { get; set; }
-
-        public string Author { get; set; }
-
-        public string Keywords { get; set; }
-
-        public string Description { get; set; }
-
-        [DisplayName("Custom meta fields")]
-        [UIHintAttribute("Dictionary")]
-        [Description("Custom fields for HTML meta values")]
-        public IDictionary<string, string> Customs
-        {
-            get;
-            set;
-        }
-    }
-
+       
     [MetadataFor(typeof(CacheSettings))]
     public class CacheSettings_Metadata
     {

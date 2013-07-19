56772b131c927d230e9daa59eab39cffdfe9833e
Jifeng <jifeng@kooboo.com>
2013-07-19 15:00:39 +0800

    Fix XSS bug. #104

--- Kooboo.CMS/Kooboo.CMS.Sites/View/PageRequestContext.cs
+++ Kooboo.CMS/Kooboo.CMS.Sites/View/PageRequestContext.cs
@@ -239,7 +239,12 @@ namespace Kooboo.CMS.Sites.View
             this.Page = page.AsActual();
             this.RequestChannel = requestChannel;
 
-            this.AllQueryString = new NameValueCollection(httpContext.Request.QueryString);
+            this.AllQueryString = new NameValueCollection();
+            var queryString = httpContext.Request.QueryString;
+            foreach (var key in queryString.AllKeys)
+            {
+                AllQueryString[HttpUtility.HtmlEncode(key)] = queryString[key];
+            }
 
 
             HttpContextBase pageContext = new PageHttpContenxt(httpContext, new PageHttpRequest(httpContext.Request, "~/" + pageRequestUrl, ""));

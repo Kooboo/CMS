9611b472fb0b016a69c6d07a96eed76bccb66960
Jifeng <jifeng@kooboo.com>
2013-07-30 18:03:42 +0800

    Commit Parameter.cs

---
+++ Kooboo.CMS/Kooboo.CMS.Common/Runtime/Parameter.cs
@@ -0,0 +1,26 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kooboo.CMS.Common.Runtime
+{
+    public class Parameter
+    {
+        public Parameter(string name, object value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}

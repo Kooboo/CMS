Kooboo/CMS

commit 68f18617319b08c1d9d9b063395f5b77722566fc

Enable the feature #73: Able to publish content via metaweblog api.

Kooboo.CMS/Kooboo.CMS.Web/KoobooCMSHttpApplicationEvents.cs
@@ -48,7 +48,11 @@ namespace Kooboo.CMS.Web
         #region RegisterRoutes
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.Add(new Route("api/metaweblog", null, null,
+     new Kooboo.CMS.Web.Interoperability.MetaWeblog.MetaWeblogRouteHandler()));
+
             Kooboo.Web.Mvc.Routing.RouteTableRegister.RegisterRoutes(routes);
+
 
             ModelMetadataProviders.Current = new KoobooDataAnnotationsModelMetadataProvider();
 

Kooboo.CMS/Kooboo.CMS.Content/Kooboo.CMS.Content/Services/TextContentManager.cs
@@ -80,7 +80,7 @@ namespace Kooboo.CMS.Content.Services
         public string Value
         {
             get
-            {                
+            {
                 return string.Join(",", Contents.Select(it => it.UUID + ":" + it.FolderName).ToArray());
             }
         }
@@ -148,7 +148,7 @@ namespace Kooboo.CMS.Content.Services
 
         public virtual ContentBase Update(Repository repository, TextFolder folder, string uuid, NameValueCollection values, string userid = "")
         {
-            return Update(repository, folder, uuid, values, null, DateTime.Now, null, null);
+            return Update(repository, folder, uuid, values, null, DateTime.Now, null, null, userid);
         }
         public virtual ContentBase Update(Repository repository, TextFolder folder, string uuid, NameValueCollection values, HttpFileCollectionBase files,
          DateTime modificationDate, IEnumerable<TextContent> addedCateogries, IEnumerable<TextContent> removedCategories, string userid = "", bool enableVersion = true)

Kooboo.CMS/Kooboo.CMS.Web/Areas/Contents/Controllers/MetaweblogController.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Kooboo.CMS.Content.Interoperability.MetaWeblog;
+using Kooboo.CMS.Web.Interoperability.MetaWeblog;
 
 namespace Kooboo.CMS.Web.Areas.Contents.Controllers
 {
@@ -22,7 +22,7 @@ namespace Kooboo.CMS.Web.Areas.Contents.Controllers
 
         public virtual ActionResult Index(string repositoryName)
         {
-            IHttpHandler httpHandler = new MetaWeblogAPIHandler() { RepositoryName = repositoryName };
+            IHttpHandler httpHandler = new MetaWeblogAPIHandler(repositoryName);
             httpHandler.ProcessRequest(System.Web.HttpContext.Current);
             return null;
         }

Kooboo.CMS/Kooboo.CMS.Web/Cms_Data/Account/Users/admin.config
@@ -7,7 +7,7 @@
 	<PasswordSalt>aKEwnA2Riwo4SCzyYFfzFw==</PasswordSalt>
 	<UICulture>en-US</UICulture>
 	<UtcLastLockoutDate i:nil="true" />
-	<UtcLastLoginDate>2013-08-12T01:14:48.4183906Z</UtcLastLoginDate>
+	<UtcLastLoginDate>2013-08-13T05:06:09.6686063Z</UtcLastLoginDate>
 	<UtcLastPasswordChangedDate i:nil="true" />
 	<Email>admin@admin.com</Email>
 	<Password>FYOL44w+oWRo53/9NQCnqa8RS00=</Password>

Kooboo.CMS/Kooboo.CMS.Web/Interoperability/MetaWeblog/MetaWeblogAPIHandler.cs
@@ -23,10 +23,18 @@ using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Text;
-namespace Kooboo.CMS.Content.Interoperability.MetaWeblog
+namespace Kooboo.CMS.Web.Interoperability.MetaWeblog
 {
     public class MetaWeblogAPIHandler : XmlRpcService, IMetaWeblog, IBlogger
     {
+        #region .ctor
+        public MetaWeblogAPIHandler(string repositoryName)
+        {
+            this.repositoryName = repositoryName;
+        }
+        #endregion
+
+        #region MetaWeblogHelper
         private static class MetaWeblogHelper
         {
             public static string CompositePostId(TextContent content)
@@ -41,6 +49,9 @@ namespace Kooboo.CMS.Content.Interoperability.MetaWeblog
                 return s[1];
             }
         }
+        #endregion
+
+        #region fields
         private string repositoryName;
         public string RepositoryName
         {
@@ -68,6 +79,9 @@ namespace Kooboo.CMS.Content.Interoperability.MetaWeblog
                 return Repository.Current;
             }
         }
+        #endregion
+
+        #region CheckUserPassword
         private void CheckUserPassword(string userName, string password)
         {
             if (Repository == null)
@@ -92,6 +106,7 @@ namespace Kooboo.CMS.Content.Interoperability.MetaWeblog
                 throw new KoobooException("The user have no permission to edit content.".Localize());
             }
         }
+        #endregion
 
         #region IMetaWeblog Members
 
@@ -110,8 +125,9 @@ namespace Kooboo.CMS.Content.Interoperability.MetaWeblog
             values["description"] = post.description;
             values["body"] = post.description;
             values["userKey"] = userKey;
+            values["published"] = publish.ToString();
 
-            ServiceFactory.GetService<TextContentManager>().Update(Repository, textFolder, content.UUID, values);
+            ServiceFactory.GetService<TextContentManager>().Update(Repository, textFolder, content.UUID, values, username);
 
 
             var old_categories = GetCategories(textFolder, content);
@@ -262,6 +278,8 @@ namespace Kooboo.CMS.Content.Interoperability.MetaWeblog
         }
 
         #endregion
+
+        #region getUsersBlogs
         public BlogInfo[] getUsersBlogs(string appKey, string username, string password)
         {
             CheckUserPassword(username, password);
@@ -280,69 +298,7 @@ namespace Kooboo.CMS.Content.Interoperability.MetaWeblog
                 url = ""
             }).ToArray();
         }
-        //#region IBlogger Members
-
-
-
-
-        //public bool deletePost(string appKey, string postid, string username, string password, bool publish)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public object editPost(string appKey, string postid, string username, string password, string content, bool publish)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //CookComputing.Blogger.Category[] IBlogger.getCategories(string blogid, string username, string password)
-        //{
-        //    CheckUserPassword(username, password);
-
-        //    var folder = (TextFolder)(FolderHelper.Parse(Repository, blogid).AsActual());
-        //    var categories = GetFolderCategories(folder);
-
-        //    return categories.Select(it => new CookComputing.Blogger.Category()
-        //    {
-        //        categoryid = MetaWeblogHelper.CompositePostId(it),
-        //        description = it.GetSummarize(),
-        //        title = it.GetSummarize(),
-        //        htmlUrl = "",
-        //        rssUrl = ""
-        //    }).ToArray();
-        //}
-
-        //public CookComputing.Blogger.Post getPost(string appKey, string postid, string username, string password)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public CookComputing.Blogger.Post[] getRecentPosts(string appKey, string blogid, string username, string password, int numberOfPosts)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public string getTemplate(string appKey, string blogid, string username, string password, string templateType)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public UserInfo getUserInfo(string appKey, string username, string password)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public string newPost(string appKey, string blogid, string username, string password, string content, bool publish)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public bool setTemplate(string appKey, string blogid, string username, string password, string template, string templateType)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //#endregion
+        #endregion
 
         #region IBlogger Members
 

Kooboo.CMS/Kooboo.CMS.Web/Interoperability/MetaWeblog/MetaWeblogRouteHandler.cs
@@ -1,0 +1,27 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+
+using Kooboo.CMS.Content.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Kooboo.CMS.Web.Interoperability.MetaWeblog
+{
+    public class MetaWeblogRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            var repositoryName = Repository.Current == null ? "" : Repository.Current.Name;
+            return new MetaWeblogAPIHandler(repositoryName);
+        }
+    }
+}

Kooboo.CMS/Kooboo.CMS.Web/Kooboo.CMS.Web.csproj
@@ -122,7 +122,6 @@
     <Compile Include="Areas\Contents\Controllers\ManagerControllerBase.cs" />
     <Compile Include="Areas\Contents\Controllers\MediaContentController.cs" />
     <Compile Include="Areas\Contents\Controllers\MediaFolderController.cs" />
-    <Compile Include="Areas\Contents\Controllers\MetaweblogController.cs" />
     <Compile Include="Areas\Contents\Controllers\PendingWorkflowController.cs" />
     <Compile Include="Areas\Contents\Controllers\ReceivingSettingController.cs" />
     <Compile Include="Areas\Contents\Controllers\RepositoryController.cs" />
@@ -246,6 +245,10 @@
     <Compile Include="Areas\Sites\Models\ABRuleSetting_Metadata.cs" />
     <Compile Include="Areas\Sites\Models\SiteTemplate_ViewModel.cs" />
     <Compile Include="Areas\Sites\Models\QueryStringVisitRule_Metadata.cs" />
+    <Compile Include="Interoperability\MetaWeblog\BloggerAPI.cs">
+      <SubType>Code</SubType>
+    </Compile>
+    <Compile Include="Interoperability\MetaWeblog\MetaWeblogRouteHandler.cs" />
     <Compile Include="KoobooCMSHttpApplicationEvents.cs" />
     <Compile Include="Models\AllPropertiesResolver.cs" />
     <Compile Include="Models\ImportModel.cs" />
@@ -429,7 +432,6 @@
     <Compile Include="Grid2\SortableGridColumn.cs" />
     <Compile Include="HealthMonitoring\TextFileWebEventProvider.cs" />
     <Compile Include="HtmlExtensions.cs" />
-    <Compile Include="Interoperability\MetaWeblog\BloggerAPI.cs" />
     <Compile Include="Interoperability\MetaWeblog\MetaWeblogAPI.cs" />
     <Compile Include="Interoperability\MetaWeblog\MetaWeblogAPIHandler.cs" />
     <Compile Include="Json_netResult.cs" />

commit 31f70dcdbc074b2802433b2dcc0ac9fecec3d2b0

Fixed bug introduced by https://github.com/Kooboo/CMS/commit/68f18617319b08c1d9d9b063395f5b77722566fc

Kooboo.CMS/Kooboo.CMS.Web/KoobooCMSHttpApplicationEvents.cs
@@ -48,7 +48,7 @@ namespace Kooboo.CMS.Web
         #region RegisterRoutes
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.Add(new Route("api/metaweblog", null, null,
+            routes.Add(new Route("api/metaweblog", null, new RouteValueDictionary(new { controller = "nonexistingcontroller" }),
      new Kooboo.CMS.Web.Interoperability.MetaWeblog.MetaWeblogRouteHandler()));
 
             Kooboo.Web.Mvc.Routing.RouteTableRegister.RegisterRoutes(routes);
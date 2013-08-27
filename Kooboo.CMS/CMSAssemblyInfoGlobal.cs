b77a4ec16e5f10fce71c74b33b8a5fa5e6527f4a
kooboo-jifeng <jifeng@kooboo.com>
2013-08-27 19:28:35 +0800

    Build version 4.2.1.827

--- Kooboo.CMS/CMSAssemblyInfoGlobal.cs
+++ Kooboo.CMS/CMSAssemblyInfoGlobal.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Yari 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.2.1.0826")]
-[assembly: AssemblyFileVersion("4.2.1.0826")]
+[assembly: AssemblyVersion("4.2.1.0827")]
+[assembly: AssemblyFileVersion("4.2.1.0827")]

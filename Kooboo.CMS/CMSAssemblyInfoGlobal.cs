bc2b85f837609a784b98273656794e083cfe61a8
kooboo-jifeng <jifeng@kooboo.com>
2014-01-13 18:02:18 +0800

    Update the KoobooModule project template.

--- Kooboo.CMS/CMSAssemblyInfoGlobal.cs
+++ Kooboo.CMS/CMSAssemblyInfoGlobal.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Yari 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.3.0.1227")]
-[assembly: AssemblyFileVersion("4.3.0.1227")]
+[assembly: AssemblyVersion("4.3.0.0113")]
+[assembly: AssemblyFileVersion("4.3.0.0113")]

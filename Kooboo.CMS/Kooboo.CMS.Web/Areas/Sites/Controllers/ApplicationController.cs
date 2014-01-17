17bc2ac022f676114dae9a8ca584c3f05704da3c
aruss <arusslabs@gmail.com>
2014-01-17 17:34:22 +0100

    Added application management section

---
+++ Kooboo.CMS/Kooboo.CMS.Web/Areas/Sites/Controllers/ApplicationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kooboo.CMS.Web.Areas.Sites.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        //
+        // GET: /Sites/ApplicationManagment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Recycle")]
+        public ActionResult RecyclePost()
+        {
+            System.Web.Hosting.HostingEnvironment.InitiateShutdown();
+            return RedirectToAction("Index");
+        }
+    }
+}

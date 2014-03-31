62cf05967b636eba3e03887f86a1e8feda009d1d
kooboo-jifeng <jifeng@kooboo.com>
2014-03-31 17:04:48 +0800

    Build daily version.

--- Kooboo.CMS/Publish/Default/Properties/AssemblyInfo.cs
+++ Kooboo.CMS/Publish/Default/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.4.0.0326")]
-[assembly: AssemblyFileVersion("4.4.0.0326")]
+[assembly: AssemblyVersion("4.4.0.0327")]
+[assembly: AssemblyFileVersion("4.4.0.0327")]

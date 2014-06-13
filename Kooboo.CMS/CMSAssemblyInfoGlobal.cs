8ccff2d3da5a6480c7b6fbbd04a31bda837e069e
kooboo-jifeng <jifeng@kooboo.com>
2014-06-13 17:36:35 +0800

    Build 4.4.0.0613

--- Kooboo.CMS/CMSAssemblyInfoGlobal.cs
+++ Kooboo.CMS/CMSAssemblyInfoGlobal.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Yari 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.4.0.0605")]
-[assembly: AssemblyFileVersion("4.4.0.0605")]
+[assembly: AssemblyVersion("4.4.0.0613")]
+[assembly: AssemblyFileVersion("4.4.0.0613")]
--- Kooboo.CMS/Publish/Default/Properties/AssemblyInfo.cs
+++ Kooboo.CMS/Publish/Default/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.4.0.0605")]
-[assembly: AssemblyFileVersion("4.4.0.0605")]
+[assembly: AssemblyVersion("4.4.0.0613")]
+[assembly: AssemblyFileVersion("4.4.0.0613")]

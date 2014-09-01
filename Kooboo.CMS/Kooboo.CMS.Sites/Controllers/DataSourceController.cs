f2c4c85da5956922959eb6b5b59a67425a576f63
kooboo-jifeng <jifeng@kooboo.com>
2014-09-01 17:57:56 +0800

    Able to request data source data for ajax.

---
+++ Kooboo.CMS/Kooboo.CMS.Sites/Controllers/DataSourceController.cs
@@ -0,0 +1,37 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+using Kooboo.CMS.Sites.DataSource;
+using Kooboo.CMS.Sites.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Kooboo.CMS.Sites.Controllers
+{
+    public class DataSourceController : FrontControllerBase
+    {
+        DataSourceSettingManager _dataSourceSettingManager;
+        public DataSourceController(DataSourceSettingManager dataSourceSettingManager)
+        {
+            _dataSourceSettingManager = dataSourceSettingManager;
+        }
+        public ActionResult Index(string dataSourceName)
+        {
+            var dataSource = _dataSourceSettingManager.Get(Site, dataSourceName);
+
+            var dataSourceContext = new DataSourceContext(Site, null) { ValueProvider = ValueProvider };
+
+            var data = dataSource.DataSource.Execute(dataSourceContext);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}

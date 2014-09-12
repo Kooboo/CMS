4e1e66f90a53404aed54419566b2cb66ee449c24
kooboo-jifeng <jifeng@kooboo.com>
2014-09-12 16:04:36 +0800

    Discard page draft extension

---
+++ Kooboo.CMS/Kooboo.CMS.Modules/Kooboo.CMS.Modules.DiscardDraft/DiscardPageDraftToolbar.cs
@@ -0,0 +1,57 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+using Kooboo.Globalization;
+using Kooboo.CMS.Sites.Extension.UI;
+using Kooboo.CMS.Sites.Extension.UI.TopToolbar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kooboo.CMS.Modules.DiscardDraft
+{
+    [Kooboo.CMS.Common.Runtime.Dependency.Dependency(typeof(IToolbarProvider), Key = "DiscardPageDraftToolbar")]
+    public class DiscardPageDraftToolbar : IToolbarProvider
+    {
+        public Sites.Extension.UI.MvcRoute[] ApplyTo
+        {
+            get
+            {
+                return new[]{
+                    new MvcRoute()
+                {
+                    Area = "Sites",
+                    Controller = "Page",
+                    Action = "Draft"
+                }
+                };
+            }
+        }
+
+        public IEnumerable<ToolbarButton> GetButtons(System.Web.Routing.RequestContext requestContext)
+        {
+            List<ToolbarButton> buttons = new List<ToolbarButton>();
+            buttons.Add(new ToolbarButton()
+            {
+                CommandTarget = new MvcRoute() { Action = "Discard", Controller = "PageDraft", Area = "_PageDraft" },
+                CommandText = "Discard",
+                IconClass = "delete",
+                HtmlAttributes = new Dictionary<string, object>() { { "data-ajaxform", "" }, { "data-confirm", "Are you sure you want to discard this draft?".Localize() } }
+            });
+
+            return buttons;
+        }
+
+        public IEnumerable<ToolbarGroup> GetGroups(System.Web.Routing.RequestContext requestContext)
+        {
+            return new ToolbarGroup[0];
+        }
+    }
+}
---
+++ Kooboo.CMS/Kooboo.CMS.Modules/Kooboo.CMS.Modules.DiscardDraft/PageDraftController.cs
@@ -0,0 +1,45 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+using Kooboo.CMS.Common;
+using Kooboo.CMS.Sites;
+using Kooboo.CMS.Sites.Models;
+using Kooboo.CMS.Sites.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Kooboo.CMS.Modules.DiscardDraft
+{
+    public class PageDraftController : AreaControllerBase
+    {
+        private PageManager _pageManager;
+        public PageDraftController(PageManager pageManager)
+        {
+            this._pageManager = pageManager;
+        }
+        public ActionResult Discard(string uuid, string @return)
+        {
+            ModelState.Clear();
+            var data = new JsonResultData(ModelState);
+            data.RunWithTry((resultData) =>
+            {
+                var page = new Page(Site, uuid);
+
+                _pageManager.Provider.RemoveDraft(page);
+
+                data.RedirectUrl = @return;
+            });
+
+            return Json(data);
+        }
+    }
+}
